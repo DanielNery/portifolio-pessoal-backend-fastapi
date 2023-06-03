5a6cb3a84b439092487e9ddef6590291b2310e21
dnery <dnery@brasilseg.com.br>
2023-06-03 01:27:26 -0300

    fix: Corrigindo dados de conhecimentos.

--- app/documents/knowledges/knowledges.py
+++ app/documents/knowledges/knowledges.py
@@ -7,18 +7,20 @@ class KnowledgesDocument(Document):
     nm_title: str = ""
     nm_description: str = ""
     nm_image: str = ""
+    nm_icon: str = ""
     nm_certificado: str = ""
     is_active: bool = False
     date_created: datetime = datetime.now()
 
     class Settings:
-        name = "hackathons"
+        name = "knowledges"
 
     class Config:
         schema_extra = {
             "nm_certificado": "teste",
             "nm_title": "A sample content",
             "nm_description": "A sample content",
+            "nm_icon": "A sample content",
             "nm_image": "A sample content",
             "is_active": True,
             "date_created": datetime.now(),
@@ -28,19 +30,21 @@ class KnowledgesDocument(Document):
 class KnowledgesCreateDocument(Document):
     nm_title: str = Field()
     nm_description: str = Field()
+    nm_icon: str = Field()
     nm_certificado: str = Field()
     is_active: bool = False
     nm_image: str = Field()
     date_created: datetime = datetime.now()
 
     class Settings:
-        name = "hackathons"
+        name = "knowledges"
 
     class Config:
         schema_extra = {
             "nm_certificado": "teste",
             "nm_title": "A sample content",
             "nm_description": "A sample content",
+            "nm_icon": "A sample content",
             "nm_image": "A sample content",
             "is_active": True,
             "date_created": datetime.now(),
